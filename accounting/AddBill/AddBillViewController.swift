47daf5c5c040c7858053aa12eaaf4e488141faf0
kudryavka1013 <50348668+kudryavka1013@users.noreply.github.com>
2020-12-22 14:13:01 +0800

    记一笔页面，选择账户

--- accounting/AddBill/AddBillViewController.swift
+++ accounting/AddBill/AddBillViewController.swift
@@ -14,19 +14,25 @@ class AddBillViewController: UIViewController,UICollectionViewDataSource,UIColle
     // 储存将要返回的bill
     var editBill: Bill?
 
-    var selectArray:Array = Array<String>()
-    var selectType:String = String()
-    
     let expenditureArray:Array = ["三餐","交通","学习","衣服","日用品","医疗","娱乐","旅行","住房","请客送礼","零食","话费网费","汽车/加油","水电煤","其它"]
     let incomeArray:Array = ["工资","生活费","外快","股票基金","其他"]
-    let accountArray:Array = ["0","1","2","3","4","5","6","7","8","9"]
+    let accountArray:Array = ["支付宝","微信","银行卡"]
+    
+    var selectArray:Array = Array<String>()
+    var selectType:String = String()
+    var account:String = String()
+    
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var accountLabel: UILabel!
     @IBOutlet weak var selectTypeView: UICollectionView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var timeSelect: UIDatePicker!
-    @IBOutlet weak var accountSelectButton: UIView!
     @IBOutlet weak var accountSelect: UIPickerView!
     @IBAction func backButton(_ sender: UIButton) {
         self.dismiss(animated:true,completion:nil)
+    }
+    @IBAction func accountTap(_ sender: UITapGestureRecognizer) {
+        accountSelect.isHidden = !accountSelect.isHidden
     }
     
     override func viewDidLoad() {
@@ -35,6 +41,7 @@ class AddBillViewController: UIViewController,UICollectionViewDataSource,UIColle
         selectTypeView.dataSource = self
         accountSelect.delegate = self
         accountSelect.dataSource = self
+        
         //设置分类只能单选
         selectTypeView.allowsMultipleSelection = false
         (selectTypeView.collectionViewLayout as! UICollectionViewFlowLayout).estimatedItemSize = CGSize(width: 20, height: 20)
@@ -44,7 +51,12 @@ class AddBillViewController: UIViewController,UICollectionViewDataSource,UIColle
         (selectTypeView.collectionViewLayout as! UICollectionViewFlowLayout).minimumLineSpacing = 5
         //设置分类View的内边距
         (selectTypeView.collectionViewLayout as! UICollectionViewFlowLayout).sectionInset = UIEdgeInsets(top: 5,left: 20,bottom: 0,right: 20)
+        
         selectArray = expenditureArray
+        accountSelect.isHidden = true
+        account = accountArray[0]
+        accountLabel.text = account
+        amountLabel.textColor = UIColor.systemRed
         // Do any additional setup after loading the view.
     }
     
@@ -52,13 +64,17 @@ class AddBillViewController: UIViewController,UICollectionViewDataSource,UIColle
         switch segmentedControl.selectedSegmentIndex {
         case 0:
             selectArray = expenditureArray
+            amountLabel.textColor = UIColor.systemRed
             self.selectTypeView.reloadData()
+//            self.selectTypeView.layoutIfNeeded()
             selectType = selectArray[0]
             print(selectType)
             break
         case 1:
             selectArray = incomeArray
+            amountLabel.textColor = UIColor.systemGreen
             self.selectTypeView.reloadData()
+//            self.selectTypeView.layoutIfNeeded()
             selectType = selectArray[0]
             print(selectType)
             break
@@ -99,17 +115,28 @@ class AddBillViewController: UIViewController,UICollectionViewDataSource,UIColle
         cell.cellStatusWithSelected(selected: false)
     }
     
+   
+    
     // MARK: - UIPickerView DataSource
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
+    //配置选项个数
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 9
+        return accountArray.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return 34
+    //配置选项内容
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return accountArray[row]
+    }
+    
+    //配置事件选择
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        account = accountArray[row]
+        accountLabel.text = account
+        print(account)
     }
     /*
     // MARK: - Navigation
